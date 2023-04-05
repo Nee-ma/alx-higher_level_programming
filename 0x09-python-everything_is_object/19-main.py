18297b92803c8a14796c3b165c075b435ed680de
Nee-ma <neemapriscillar21@gmail.com>
2023-04-04 17:51:59 -0700

    push

--- 0x09-python-everything_is_object/19-main.py
+++ 0x09-python-everything_is_object/19-main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+copy_list = __import__('19-copy_list').copy_list
 
+my_list = [1, 2, 3]
+print(my_list)
 
-def copy_list(l):
-    """generates a copy of a list
-    """
-    if not isinstance(l, list):
-        return None
-    return l[:]
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+print(new_list is my_list)
